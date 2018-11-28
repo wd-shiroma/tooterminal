e6caa073627f10225dd018399378b2a9a306f7eb
guskma <guskma@drdr.work>
2018-11-28 16:12:47 +0900

    v2.3.0

--- config/client_info.js
+++ config/client_info.js
@@ -1,6 +1,6 @@
 let client_info = {
-    modified: (new Date('2018-11-15')),
-    version: '2.2.2',
+    modified: (new Date('2018-11-28')),
+    version: '2.3.0',
     auther: 'Guskma',
     acct: 'guskma@abyss.fun',
     website: 'https://blog.drdr.work/'
