9e99b7bd8e1496a3ae76b0d8d755200a2e4e186a
Guskma <wonderdream.hts@gmail.com>
2019-11-23 18:43:38 +0900

    v2.3.3

--- config/client_info.js
+++ config/client_info.js
@@ -1,6 +1,6 @@
 let client_info = {
-    modified: (new Date('2019-08-22')),
-    version: '2.3.2',
+    modified: (new Date('2019-11-23')),
+    version: '2.3.3',
     auther: 'Guskma',
     acct: 'guskma@abyss.fun',
     website: 'https://blog.drdr.work/'
