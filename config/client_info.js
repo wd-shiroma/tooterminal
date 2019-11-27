1247cd59ad7a404adb008ae05e8dce34d9c326a9
guskma <guskma@drdr.work>
2019-11-27 15:18:31 +0900

    fix process requesting search at mastodon over v3.0.0

--- config/client_info.js
+++ config/client_info.js
@@ -1,9 +1,7 @@
 let client_info = {
-    modified: (new Date('2019-11-23')),
-    version: '2.3.3',
+    modified: (new Date('2019-11-27')),
+    version: '2.3.4',
     auther: 'Guskma',
     acct: 'guskma@abyss.fun',
     website: 'https://blog.drdr.work/'
 };
-
-
