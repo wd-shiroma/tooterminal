74258d91f0436fca7fcf673419039ccb303ecddb
guskma <2527362+wd-shiroma@users.noreply.github.com>
2020-03-30 13:48:42 +0900

    v2.3.6

--- config/client_info.js
+++ config/client_info.js
@@ -1,6 +1,6 @@
 let client_info = {
-    modified: (new Date('2020-03-02')),
-    version: '2.3.5',
+    modified: (new Date('2020-03-30')),
+    version: '2.3.6',
     auther: 'Guskma',
     acct: 'guskma@abyss.fun',
     website: 'https://blog.drdr.work/'
