abf23ec00f9b1bd32e0f93170341f1d47ae5bbe7
guskma <guskma@drdr.work>
2020-06-15 20:04:25 +0900

    v2.3.8

--- config/client_info.js
+++ config/client_info.js
@@ -1,6 +1,6 @@
 let client_info = {
-    modified: (new Date('2020-06-10')),
-    version: '2.3.7',
+    modified: (new Date('2020-06-15')),
+    version: '2.3.8',
     auther: 'Guskma',
     acct: 'guskma@abyss.fun',
     website: 'https://blog.drdr.work/'
